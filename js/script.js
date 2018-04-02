f6c739bddb05ac702eb8bf7bf3562b5bb7ecfc19
Parkhomenko Oleg <Oleg.Parkhom@gmail.com>
2018-04-02 19:03:16 +0300

    1) Пункт меню "Информация" не имеет активного состояния; 2) Добавил localStorage и анимацию форме на главной седоны.
    1) Пункт меню "Информация" не имеет активного состояния;
    2) Добавил localStorage и анимацию форме на главной седоны.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,39 @@
-var orderButton = document.querySelector(".order-open-btn");
-var pullDownMenu = document.querySelector(".order-form");
+var orderButton = document.querySelector(".btn-action");
+var pullDownMenu = document.querySelector("form");
+var In = pullDownMenu.querySelector("[name=check-in]");
+var adults = pullDownMenu.querySelector("[name=adults]");
+var kids = pullDownMenu.querySelector("[name=kids]");
+var storage = localStorage.getItem("In");
 
-if (orderButton) {
-  if (pullDownMenu) {
-    pullDownMenu.classList.add("order-form-hide");
-  };
-  orderButton.addEventListener("click", function(event) {
-    event.preventDefault();
-    pullDownMenu.classList.toggle("order-form-hide");
-    pullDownMenu.classList.toggle("order-form-show");
-  });
-};
+pullDownMenu.classList.add("order-form-hide");
+
+orderButton.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  pullDownMenu.classList.toggle("order-form-show");
+  In.focus();
+  if (storage) {
+    In.value = storage;
+    Out.focus();
+  } else {
+    In.focus();
+  }
+});
+
+pullDownMenu.addEventListener("submit", function(evt) {
+  if ((!In.value) || (!Out.value)) {
+    evt.preventDefault();
+    pullDownMenu.classList.remove("order-form-error");
+    pullDownMenu.offsetWidth = pullDownMenu.offsetWidth;
+    pullDownMenu.classList.add("order-form-error");
+  } else {
+    localStorage.setItem("In", In.value);
+  }
+});
+
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    if (pullDownMenu.classList.contains("order-form-show")) {
+      pullDownMenu.classList.remove("order-form-show");
+    }
+  }
+});
